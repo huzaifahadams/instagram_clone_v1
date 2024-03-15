dbd63fe768f0b70d2d4deeb744f16eaba49f82b4
huzaifahadams <huzaifah.adams@gmail.com>
2024-03-15 11:29:19 +0300

    done with the google and facebbok linking processing step 2 for now

---
+++ lib/state/auth/constants/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+
+@immutable
+class Constants {
+  static const accoubtExistswithDifferentCrdentual =
+      'Account-exists-with-diffrent-credentaiks';
+
+  static const goolgeCom = 'google.com';
+  static const emailScope = 'email';
+
+  const Constants._();
+}
